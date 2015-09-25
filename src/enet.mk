19eaa63ea4e13c90ccd59b43ca9033de600a39a5
ooxi <violetland@mail.ru>
2015-09-25 12:37:26 +0200

    enet...

--- src/enet.mk
+++ src/enet.mk
@@ -12,10 +12,7 @@ $(PKG)_URL      := $($(PKG)_GITHUB)/archive/$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := gcc
 
 
-define $(PKG)_BUILD
-
-    echo 'add_definitions(-DENET_DLL)' >> '$(1)/CMakeLists.txt'
-
+define $(PKG)_BUILD_STATIC
 
     # Configure
     mkdir '$(1).build'
@@ -50,3 +47,43 @@ define $(PKG)_BUILD
         -lenet -lws2_32 -lwinmm
 
 endef
+
+
+define $(PKG)_BUILD_SHARED
+
+    echo 'abc' > /tmp/xyz
+
+    # Configure
+    mkdir '$(1).build'
+    cd '$(1).build' && cmake \
+      -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+      '$(1)'
+
+
+    # Build
+    $(MAKE) -C '$(1).build' -j '$(JOBS)'
+
+
+    # Install include files
+    $(INSTALL) -D '$(1)/include/enet/callbacks.h' '$(PREFIX)/$(TARGET)/include/enet/callbacks.h'
+    $(INSTALL) -D '$(1)/include/enet/enet.h'      '$(PREFIX)/$(TARGET)/include/enet/enet.h'
+    $(INSTALL) -D '$(1)/include/enet/list.h'      '$(PREFIX)/$(TARGET)/include/enet/list.h'
+    $(INSTALL) -D '$(1)/include/enet/protocol.h'  '$(PREFIX)/$(TARGET)/include/enet/protocol.h'
+    $(INSTALL) -D '$(1)/include/enet/time.h'      '$(PREFIX)/$(TARGET)/include/enet/time.h'
+    $(INSTALL) -D '$(1)/include/enet/types.h'     '$(PREFIX)/$(TARGET)/include/enet/types.h'
+    $(INSTALL) -D '$(1)/include/enet/unix.h'      '$(PREFIX)/$(TARGET)/include/enet/unix.h'
+    $(INSTALL) -D '$(1)/include/enet/utility.h'   '$(PREFIX)/$(TARGET)/include/enet/utility.h'
+    $(INSTALL) -D '$(1)/include/enet/win32.h'     '$(PREFIX)/$(TARGET)/include/enet/win32.h'
+
+    # Install library
+    $(INSTALL) -D '$(1).build/libenet.a'            '$(PREFIX)/$(TARGET)/lib/libenet.a'
+
+
+    # Build test executable
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-enet.exe' \
+        -lenet -lws2_32 -lwinmm
+
+endef
+
