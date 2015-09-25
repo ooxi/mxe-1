cafdcb32e0539fb2231ca8abeaad4213f3f8ca74
ooxi <violetland@mail.ru>
2015-09-25 12:14:31 +0200

    enet...

--- src/enet.mk
+++ src/enet.mk
@@ -17,13 +17,12 @@ define $(PKG)_BUILD
     # Configure
     mkdir '$(1).build'
     cd '$(1).build' && cmake \
-      $(if $(BUILD_STATIC), , -DENET_DLL) \
       -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
       '$(1)'
 
 
     # Build
-    $(MAKE) -C '$(1).build' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build' -j '$(JOBS)' $(if $(BUILD_STATIC), , -DENET_DLL)
 
 
     # Install include files
